077ddb1b23ecae4deac285f8f8b621093f949b73
Marcos Marado <mindboosternoori@gmail.com>
2016-10-09 19:13:30 +0100

    Enhanced the parser to also fetch epub's names
    ...and an overall re-write of the script in the process.

--- adamaSHtor.sh
+++ adamaSHtor.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
 
-# start by fetching the books
-/bin/bash fetch.sh
-# and then separate epubs and mobi's, giving them proper filenames while we're at it
-/bin/bash process.sh
+# Fetch the HTML with the list of books
+rm -f index.html # in case we have an older version around
+wget http://projectoadamastor.org/listageral/
+
+# parse the HTML list
+nomes=$(cat index.html|hxnormalize -x -l 1000|hxselect tbody tr td:first-child a -c -s "\n")
+links=$(
+	for a in $(
+		cat index.html| \
+		hxnormalize -x -l 1000| \
+		hxselect tbody tr td:nth-child\(4\) -c -s "\n"
+	); do
+		echo "$a"|grep href|cut -d\" -f2;
+	done;
+);
+
+let i=0;
+parsed=$(
+	echo "$nomes" | while read -r n; do
+		let i++;
+		l=$(echo "$links"|head -n $i|tail -n 1);
+		echo "$l $n";
+	done
+);
+
+epubs=$(echo "$parsed"|grep -v MOBI)
+mobi=$( echo "$parsed"|grep    MOBI)
+
+# cleaning up whatever is in the directory already
+rm -rf books
+mkdir books
+
+# fetch the actual books
+## epubs...
+mkdir books/epubs
+cd books/epubs
+echo "$epubs"|while read -r b; do
+	link=$(echo "$b"|cut -d" " -f1)
+	name=$(echo "$b"|cut -d" " -f2-|cut -d\& -f1)
+	wget $link -o /dev/null -O "$name.epub"
+done
+## ...and mobis
+mkdir ../mobi
+cd ../mobi
+echo "$mobi"|while read -r b; do
+	link=$(echo "$b"|cut -d" " -f1)
+	name=$(echo "$b"|cut -d" " -f2-|cut -d\& -f1)
+	wget $link -o /dev/null -O "$name.mobi"
+done
--- fetch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget http://projectoadamastor.org/listageral/
-for s in $(cat index.html |hxnormalize -x -l 1000|hxselect tbody|hxselect a.wpdm-download-link); do echo $s|grep href; done|cut -d\" -f2 > links
-# cleaning up whatever is in the directory already
-rm -rf books/*
-cd books
-for i in $(cat ../links); do wget $i -o /dev/null; done
