6c189fc860cf061fad148283e484f3eb110f8908
Marcos Marado <mindboosternoori@gmail.com>
2017-09-07 19:41:03 +0100

    Added DEBUG flag

--- adamaSHtor.sh
+++ adamaSHtor.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# DEBUG=1
+
 # Fetch the HTML with the list of books
+[[ "$DEBUG" ]] && echo "DEBUG: 1";
 rm -f index.html # in case we have an older version around
+[[ "$DEBUG" ]] && echo "DEBUG: 2";
 wget http://projectoadamastor.org/listageral/
 
+[[ "$DEBUG" ]] && echo "DEBUG: 3";
 # parse the HTML list
 nomes=$(cat index.html|hxnormalize -x -l 1000|hxselect tbody tr td:first-child a -c -s "\n")
+[[ "$DEBUG" ]] && echo "DEBUG: 4";
 links=$(
 	for a in $(
 		cat index.html| \
@@ -16,6 +22,7 @@ links=$(
 	done;
 );
 
+[[ "$DEBUG" ]] && echo "DEBUG: 5";
 let i=0;
 parsed=$(
 	echo "$nomes" | while read -r n; do
@@ -25,6 +32,7 @@ parsed=$(
 	done
 );
 
+[[ "$DEBUG" ]] && echo "DEBUG: 6";
 epubs=$(echo "$parsed"|grep -v MOBI)
 mobi=$( echo "$parsed"|grep    MOBI)
 
@@ -36,12 +44,14 @@ mkdir books
 ## epubs...
 mkdir books/epubs
 cd books/epubs
+[[ "$DEBUG" ]] && echo "DEBUG: 7";
 echo "$epubs"|while read -r b; do
 	link=$(echo "$b"|cut -d" " -f1)
 	name=$(echo "$b"|cut -d" " -f2-|cut -d\& -f1)
 	wget $link -o /dev/null -O "$name.epub"
 done
 ## ...and mobis
+[[ "$DEBUG" ]] && echo "DEBUG: 8";
 mkdir ../mobi
 cd ../mobi
 echo "$mobi"|while read -r b; do
@@ -49,3 +59,5 @@ echo "$mobi"|while read -r b; do
 	name=$(echo "$b"|cut -d" " -f2-|cut -d\& -f1)
 	wget $link -o /dev/null -O "$name.mobi"
 done
+
+[[ "$DEBUG" ]] && echo "DEBUG: 9";
